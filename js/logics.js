ba3cb4c51755074a7384c5ccdac977dc6580ad91
hman1 <hman@walmartlabs.com>
2015-08-13 17:18:08 -0700

    merge master

---
+++ js/logics.js
@@ -0,0 +1,101 @@
+var App = App || {};
+App.Position = function Position(row, col){
+	this.row = row;
+	this.col = col;
+};
+App.Logics = {
+  checkAllValid: function(){
+    var helper = {};
+    var visited = [];
+    var errorPositions = [];
+    var boardRowLength = App.board.length;
+    if(boardRowLength != App.SUDOKU_LENGTH) return false;
+    var boardColLength = App.board[0].length;
+    if(boardColLength != App.SUDOKU_LENGTH) return false;
+    
+    // initiate visited array
+    for (var i = 0; i < App.SUDOKU_LENGTH; i++){
+    	visited[i] = new Array(App.SUDOKU_LENGTH);
+    	for (var m = 0; m < App.SUDOKU_LENGTH; m++){
+    		visited[i][m] = false;
+    	}
+    }
+    //check row
+    for(var row = 0; row < boardRowLength; row++){
+      helper = {};
+      for(var col = 0; col < boardColLength; col++){
+        var curNum = App.board[row][col];
+        if (curNum){
+          if (_.has(helper, curNum) && !visited[row][col]) {
+          	visited[row][col] = true;
+          	errorPositions.push(new App.Position(row, col));
+          }else {
+          	helper[curNum] = 1;
+          }
+        }
+      }
+    }
+    //check col
+    for(var col=0;col<boardColLength;col++){
+      helper = {};
+      for(var row=0;row<boardRowLength;row++){
+        var curNum = App.board[row][col];
+        if (curNum){
+          if (_.has(helper, curNum) && !visited[row][col]) {
+          	visited[row][col] = true;
+          	errorPositions.push(new App.Position(row, col));
+          }else {
+          	helper[curNum] = 1;
+          }
+        }
+      }
+    }
+    //check 3*3 grid
+    for(var row = 0; row < boardRowLength; row += App.SUB_SUDOKU_LENGTH) {
+      for(var col = 0; col < boardColLength; col += App.SUB_SUDOKU_LENGTH) {
+        helper = {};
+        for(var subRow = 0; subRow < 3; ++subRow) {
+          for(var subCol = 0; subCol < 3; ++subCol) {
+            var curNum = App.board[row + subRow][col + subCol];
+            if (curNum){
+              if (_.has(helper, curNum) && !visited[row + subRow][col + subCol]) {
+              	visited[row][col] = true;
+              	errorPositions.push(new App.Position(row + subRow, col + subCol));
+              }else {
+              	helper[curNum] = 1;
+              }
+            }
+          }
+        }
+      }
+    }
+    return errorPositions;
+  },
+  
+  checkCurrentRowColValid: function(row, col, value){
+  	for ( var r = 0; r < App.SUDOKU_LENGTH; r++){
+  		if (r != row && App.board[r][col] == value) return false;
+  	}
+  	for ( var c = 0; c < App.SUDOKU_LENGTH; c++){
+  		if (c != col && App.board[row][c] == value) return false;
+  	}
+  	return true;
+  },
+  
+  // Solve sudoku
+  solve: function(){
+  	for (var row = 0; row < App.SUDOKU_LENGTH; row++){
+  		for (var col = 0; col < App.SUDOKU_LENGTH; col++){
+  			if (!App.board[row][col]){
+  				for (var num = 1; num <= App.SUDOKU_LENGTH; num++){
+  					App.board[row][col] = num;
+  					if (_.isEmpty(this.checkAllValid()) && this.solve()) return true;
+  					App.board[row][col] = null;
+  				}
+  				return false;
+  			}
+  		}
+  	}
+  	return true;
+	}
+};
---
+++ js/view.js
@@ -0,0 +1,158 @@
+var App = App || {};
+
+App.View = {
+  // default grid from http://en.wikipedia.org/wiki/File:Sudoku-by-L2G-20050714.svg
+  defaultGrid: [
+    [5, 3, null, null, 7, null, null, null, null],
+    [6, null, null, 1, 9, 5, null, null, null],
+    [null, 9, 8, null, null, null, null, 6, null],
+    [8, null, null, null, 6, null, null, null, 3],
+    [4, null, null, 8, null, 3, null, null, 1],
+    [7, null, null, null, 2, null, null, null, 6],
+    [null, 6, null, null, null, null, 2, 8, null],
+    [null, null, null, 4, 1, 9, null, null, 5],
+    [null, null, null, null, 8, null, null, 7, 9]
+  ],
+  ONE_KEY_CODE: 49,
+  NINE_KEY_CODE: 57,
+
+	// initiate a default game, if we want to make a board generator in the future, we just send the array as an argument here
+  gameInit: function(){
+  	if (App.isComplete) App.isComplete = false;
+   	this.renderBoard(this.defaultGrid);
+   	this.clickEventBind();
+   	this.columnHover();
+   	this.checkEmpty();   	
+  },
+  
+  // check current empty slots
+  checkEmpty: function(){
+  	this.remainSlots = 0;
+   	for (var row = 0; row < App.board.length; row++){
+   		for (var col = 0; col < App.board[0].length; col++){
+   			if (!App.board[row][col]) this.remainSlots++;
+   		}
+   	}
+  },
+  // restart or rerender the board
+  renderBoard: function(board){
+  	var $table = _.template($('#table_template').html(), { board: board });
+    var $main = $(".sudoku-table.main");
+    $main.empty().append($table);
+    App.board = $.extend(true, [], board);  	
+  },
+  
+  // mouse hover effect, showing all column which is in the same row or same column
+  columnHover: function(){
+  	$(".sudoku-column").hover(
+  		function(){
+  			$(".sudoku-column[row =" + $(this).attr('row') + "]").addClass('hover');
+  			$(".sudoku-column[col =" + $(this).attr('col') + "]").addClass('hover');
+  		}, function(){
+  			$(".sudoku-column[row =" + $(this).attr('row') + "]").removeClass('hover');
+  			$(".sudoku-column[col =" + $(this).attr('col') + "]").removeClass('hover');
+  		}
+  	)
+  },
+ 
+ 	// mouse click events binding
+ 	restartBtnHandler: function(){
+ 		App.View.gameInit();
+ 	},
+ 	
+ 	// handler for check button
+ 	checkBtnHandler: function(){
+ 		if (App.isComplete){
+ 			alert("Sudoku has been solved.");
+ 			return;
+ 		}
+ 		var errors = App.Logics.checkAllValid();
+  	if (_.isEmpty(errors)){
+  		alert("Sudoku looks good, please continue");
+  	}else{
+  		alert("Duplicates found! Please resolve it!");
+  	}
+ 	},
+ 	
+ 	// handler for clicking the column
+ 	columnHandler: function(event){
+ 			var $target = $(event.currentTarget);
+  		var $span = $target.find('span');
+  		var row = parseInt($target.attr('row'));
+  		var col = parseInt($target.attr('col'));
+  		if (!$target.hasClass('default')){ 		
+  			var $input = $target.find('input');
+  			$span.hide();
+  			$input.show().focus();
+  			$input.unbind('keyup focusout');
+  			// If there is error in the column, once the user click, we clear the input for the user
+  			if ($target.hasClass('error')) $input.val('');
+  			$input.keyup(function(event){
+  				var keyCode = event.keyCode;
+  				if (event.keyCode >= App.View.ONE_KEY_CODE && event.keyCode <= App.View.NINE_KEY_CODE) {
+  					var value = $input.val();
+  					App.board[row][col] = parseInt(value);
+  					App.View.checkEmpty();				
+  					// If there is duplicate, indicate error
+  					if (!App.Logics.checkCurrentRowColValid(row, col, parseInt(value))) {
+  						$target.addClass('error');
+  					}else{
+  						$target.removeClass('error');
+  					};
+  					
+  					// If no errors exist and there are no more empty slot, it's a win
+  					if (App.View.remainSlots == 0 && $('.error').length == 0){
+  						alert('Congratulations! You have solved the sudoku!');
+  						App.View.gameInit();
+  						return;
+  					}
+  					$span.text(value);
+  					$span.show();
+  					$input.hide();
+  				}else{
+  					App.board[row][col] = null;
+  					$input.val('');
+  					$target.removeClass('error');
+  				}
+  			});
+  			$input.focusout(function(event){
+  				var value = $(event.currentTarget).val().trim();
+  				if (_.isEmpty(value)){
+  					App.board[row][col] = null;
+  					if ($target.hasClass('error')) $target.removeClass('error');
+  				}
+  				$span.text(value);
+  				$span.show();
+  				$input.hide();
+  			});
+  		}
+ 	},
+ 	
+ 	// solve button handler
+ 	solveBtnHandler: function(){
+ 		App.board = $.extend(true, [], App.View.defaultGrid); 
+ 		if (App.Logics.solve()){
+ 			App.isComplete = true;
+ 			App.View.renderBoard(App.board);
+ 		}else{
+ 			alert('No solution for this placement....:(');
+ 		}
+ 	},
+ 	
+ 	// initiate event binding
+  clickEventBind: function(){
+  	var self = this;
+  	var $restart = $('#restart');
+  	var $check = $('#check');
+  	var $sudokuColumn = $('.sudoku-column');
+  	var $solution = $('#solve');
+  	$restart.unbind();
+  	$check.unbind();
+  	$sudokuColumn.unbind();
+  	$solution.unbind();
+  	$restart.click(self.restartBtnHandler);
+  	$check.click(self.checkBtnHandler);
+  	$sudokuColumn.click(self.columnHandler);
+  	$solution.click(self.solveBtnHandler);
+  }
+};
